2583661562f0a7c9e8f0b3222da7e892cfe85880
Elia Dania Serepina <80114768+eledans@users.noreply.github.com>
2023-09-14 23:41:13 +0700

    Add files via upload

---
+++ day-2/day-2.py
@@ -0,0 +1,7 @@
+# String
+print("Hello"[1])
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
---
+++ day-2/tip_calc.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = float(input("How many people to split the bill? "))
+pay = round((bill * (1 + percentage/100))/people, 2)
+print(f"Each person should pay: ${pay}")
